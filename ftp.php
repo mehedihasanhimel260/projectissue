1c432538f40ea11391293a64329c7fd2ce329116
Mehedi Hasan <mehedihasanhimel260@gamil.com>
2025-11-03 16:07:39 +0600

    first commit

---
+++ ftp.php
@@ -0,0 +1,745 @@
+
+    {
+      "name": "paradiesbelize.de",
+      "host": "185.87.21.78",
+      "protocol": "ftp",
+      "port": 21,
+      "username": "ftp_h10596-wz2w2mfc",
+      "password": "R5fv?6Hz?qKs8xlq",
+      "remotePath": "/httpdocs",
+      "uploadOnSave": true,
+      "useTempFile": false,
+      "openSsh": false
+    }
+  
+  
+  Ticket ID #1926138773
+    {
+      "name": "connect.seanergy.app",
+      "host": "62.72.59.178",
+      "protocol": "sftp",
+      "port": 22,
+      "username": "bugfinder",
+      "password": "Nehil@1978*01",
+      "remotePath": "/home/seanergyapp/apps/connect",
+      "uploadOnSave": true,
+      "useTempFile": false,
+      "openSsh": false
+    }
+	
+	 Ticket ID #1625821794 
+	{
+	  "name": "travelvichar.com",
+	  "host": "62.72.43.250",
+	  "protocol": "ftp",
+	  "port": 21,
+	  "username": "travelvichar",
+	  "password": "Tarun@@123#",
+	  "remotePath": "/public_html",
+	  "uploadOnSave": true,
+	  "useTempFile": false,
+	  "openSsh": false
+	}	 
+	
+	Ticket ID #1657252119 
+	{
+	  "name": "growthsprintx.online",
+	  "host": "62.72.50.243",
+	  "protocol": "ftp",
+	  "port": 21,
+	  "username": "u140487350.growthsprintx.online",
+	  "password": "m4KCD>W6nu1y@$#!",
+	  "remotePath": "/public_html",
+	  "uploadOnSave": true,
+	  "useTempFile": false,
+	  "openSsh": false
+	}
+	
+	  Ticket ID #812617432
+
+    {
+      "name": "growwbitcoin.com",
+      "host": "157.245.98.24",
+      "protocol": "sftp",
+      "port": 22,
+      "username": "growwbitcoin",
+      "password": "Growwbitcoin@1",
+      "remotePath": "/public_html",
+      "uploadOnSave": true,
+      "useTempFile": false,
+      "openSsh": false
+    }
+	
+
+
+	  Ticket ID #358427894
+
+    {
+      "name": "stakeearn.io",
+      "host": "ftp.stakeearn.io",
+      "protocol": "ftp",
+      "port": 21,
+      "username": "u879940743.bugfinder",
+      "password": "3me/13nKx=fMZP*u",
+      "remotePath": "/public_html",
+      "uploadOnSave": true,
+      "useTempFile": false,
+      "openSsh": false
+    }
+
+	  Ticket ID #30484486
+
+    {
+      "name": "bitcrypto.cloud",
+      "host": "46.202.172.94",
+      "protocol": "ftp",
+      "port": 21,
+      "username": "u629877087",
+      "password": "Pass@3210000",
+      "remotePath": "/domains/bitcrypto.cloud/public_html",
+      "uploadOnSave": true,
+      "useTempFile": false,
+      "openSsh": false
+    }
+	
+	  Ticket ID #717051383
+
+    {
+      "name": "eyadsmm.com",
+      "host": "ftp.eyadsmm.com",
+      "protocol": "ftp",
+      "port": 21,
+      "username": "u742971103.eyadsmm.com",
+      "password": "9h0=M]wq?8z#tAR*",
+      "remotePath": "/public_html",
+      "uploadOnSave": true,
+      "useTempFile": false,
+      "openSsh": false
+    }
+	
+	 Ticket ID #1081749569
+
+	{
+		"name": "clickmoneyhub",
+		"host": "31.187.72.84",
+		"protocol": "ftp",
+		"port": 21,
+		"username": "clickmoneyhub",
+		"password": "$BpowkXy1G^5bjv5",
+		"remotePath": "/",
+		"uploadOnSave": true,
+		"useTempFile": false,
+		"openSsh": false
+	}
+	
+	  Ticket ID #190565480
+
+    {
+      "name": "okupi.exchange",
+      "host": "139.84.174.60",
+      "protocol": "sftp",
+      "port": 22,
+      "username": "okupiexchange",
+      "password": "OkDev@2244#",
+      "remotePath": "/public_html",
+      "uploadOnSave": true,
+      "useTempFile": false,
+      "openSsh": false
+    }
+	
+	Ticket ID #47919103
+    {
+      "name": "alqanibrokers.com",
+      "host": "88.223.84.67",
+      "protocol": "ftp",
+      "port": 21,
+      "username": "u728672845",
+      "password": "&B1Vu>!0EiVg7?*g",
+      "remotePath": "/domains/alqanibrokers.com/public_html",
+      "uploadOnSave": true,
+      "useTempFile": false,
+      "openSsh": false
+    }	
+	
+	Ticket ID #1124589856
+    {
+      "name": "online-shopcart.com",
+      "host": "online-shopcart.com",
+      "protocol": "ftp",
+      "port": 21,
+      "username": "shopcart@online-shopcart.com",
+      "password": "kFWWAWdgGaSNhuUN4emP",
+      "remotePath": "/public_html",
+      "uploadOnSave": true,
+      "useTempFile": false,
+      "openSsh": false
+    }
+	Ticket ID #1135762275
+
+	{
+		"name": "clickmoneyhub",
+		"host": "92.113.19.112",
+		"protocol": "ftp",
+		"port": 21,
+		"username": "u368735083",
+		"password": "O9o&Xgxy?]5AUdAl",
+		"remotePath": "/",
+		"uploadOnSave": true,
+		"useTempFile": false,
+		"openSsh": false
+	}
+	Ticket ID #189057766
+
+	{
+		"name": "velopay.net",
+		"host": "162.0.236.80",
+		"protocol": "ftp",
+		"port": 21,
+		"username": "morebug_velopay.net",
+		"password": "9936XYs64e",
+		"remotePath": "/",
+		"uploadOnSave": true,
+		"useTempFile": false,
+		"openSsh": false
+	}	
+	
+	Ticket ID #415849752
+
+	{
+		"name": "fistremit.com",
+		"host": "ftp.jhm.gvh.mybluehost.me",
+		"protocol": "ftp",
+		"port": 21,
+		"username": "bugfinder@fistremit.com",
+		"password": "JugW+6I+YSSH",
+		"remotePath": "/public_html",
+		"uploadOnSave": true,
+		"useTempFile": false,
+		"openSsh": false
+	}
+	
+	 Ticket ID #1224288108
+
+
+    {
+      "name": "growwbitcoin.com",
+      "host": "157.245.98.24",
+      "protocol": "sftp",
+      "port": 22,
+      "username": "growwinvest",
+      "password": "GrowwBTC123",
+      "remotePath": "/public_html",
+      "uploadOnSave": true,
+      "useTempFile": false,
+      "openSsh": false
+    }
+	
+	Ticket ID #1217254844
+
+	{
+		"name": "fistremit.com",
+		"host": "ftp.elpasonepal.com",
+		"protocol": "ftp",
+		"port": 21,
+		"username":"bugfinder@elpasonepal.com",
+		"password": ",g&O]MUm(4UorRTvcP",
+		"remotePath": "/",
+		"uploadOnSave": true,
+		"useTempFile": false,
+		"openSsh": false
+	},
+	{
+		"name": "elpasonepal.com",
+		"host": "ftp.elpasonepal.com",
+		"protocol": "ftp",
+		"port": 21,
+		"username":"bugfinder@elpasonepal.com",
+		"password": ",g&O]MUm(4UorRTvcP",
+		"remotePath": "/public_html",
+		"uploadOnSave": true,
+		"useTempFile": false,
+		"openSsh": false
+	}
+	
+	Ticket ID #1229341705
+	
+	{
+		"name": "snstree.kr",
+		"host": "snstree.kr",
+		"protocol": "sftp",
+		"port": 22,
+		"username":"snstree",
+		"password": "wh112445",
+		"remotePath": "/var/www/snstree.kr",
+		"uploadOnSave": true,
+		"useTempFile": false,
+		"openSsh": false
+	}
+	
+	
+	Ticket ID #826628599
+	
+	{
+		"name": "miningcapital.net",
+		"host": "server347.web-hosting.com",
+		"protocol": "ftp",
+		"port": 21,
+		"username":"minikcds",
+		"password": "4d9sUJH6HoGw",
+		"remotePath": "/public_html",
+		"uploadOnSave": true,
+		"useTempFile": false,
+		"openSsh": false
+	}
+	
+	Ticket ID #2103070202
+
+	{
+		"name": "partycenter.se",
+		"host": "ftp.partycenter.se",
+		"protocol": "ftp",
+		"port": 21,
+		"username":"listplace@partycenter.se",
+		"password": "listplace00",
+		"remotePath": "/",
+		"uploadOnSave": true,
+		"useTempFile": false,
+		"openSsh": false
+	}	
+	Ticket ID #2103070202
+
+	{
+		"name": "turkeyholidaytours.com",
+		"host": "78.111.106.35",
+		"protocol": "ftp",
+		"port": 21,
+		"username":"turkeyholidaytours.c_p9anuzuyp4c",
+		"password": "MLt?@3ecYy8f4unx",
+		"remotePath": "/",
+		"uploadOnSave": true,
+		"useTempFile": false,
+		"openSsh": false
+	}
+	
+	
+	Ticket ID #263406315
+
+	
+	{
+		"name": "flamfusion.com",
+		"host": "78.111.106.35",
+		"protocol": "ftp",
+		"port": 21,
+		"username":"flamfusion.com_bygaq92pvml",
+		"password": "MLt?@3ecYy8f4unx",
+		"remotePath": "/",
+		"uploadOnSave": true,
+		"useTempFile": false,
+		"openSsh": false
+	}
+	
+	Ticket ID #1325139106
+	
+	{
+		"name": "enviomais.com",
+		"host": "ftp.enviomais.com",
+		"protocol": "ftp",
+		"port": 21,
+		"username":"u793467765.bugfinder",
+		"password": "Bugfinder2025#",
+		"remotePath": "/",
+		"uploadOnSave": true,
+		"useTempFile": false,
+		"openSsh": false
+	}
+	
+	Ticket ID #1315129229
+	
+	{
+		"name": "enviomais.com",
+		"host": "139.84.174.60",
+		"protocol": "sftp",
+		"port": 22,
+		"username":"okupip2p",
+		"password": "Okupi@768",
+		"remotePath": "/",
+		"uploadOnSave": true,
+		"useTempFile": false,
+		"openSsh": false
+	}
+	
+	
+	{
+		"name": "carpcoin.net",
+		"host": "azimut01.ftp.tools",
+		"protocol": "ftp",
+		"port": 21,
+		"username":"azimut01_ftp",
+		"password": "42bXEe5xD8",
+		"remotePath": "/carpcoin.net/www",
+		"uploadOnSave": true,
+		"useTempFile": false,
+		"openSsh": false
+	}	
+	
+	
+	Ticket ID #1325139106
+
+	{
+		"name": "enviomais.com",
+		"host": "u793467765",
+		"protocol": "ftp",
+		"port": 21,
+		"username":"u793467765",
+		"password": "Angol@2025#123",
+		"remotePath": "/domains/enviomais.com/public_html",
+		"uploadOnSave": true,
+		"useTempFile": false,
+		"openSsh": false
+	}
+	
+	
+		
+	Ticket ID #1551096621
+	
+	
+	{
+		"name": "primevest.money",
+		"host": "94.23.171.169",
+		"protocol": "ftp",
+		"port": 21,
+		"username":"primevest",
+		"password": "gQ6mS5tL0h",
+		"remotePath": "/www/primevest.money",
+		"uploadOnSave": true,
+		"useTempFile": false,
+		"openSsh": false
+	}		
+		
+		
+		
+	Ticket ID #1038099956
+	{
+		"name": "gamecredit.trade",
+		"host": "94.23.171.169",
+		"protocol": "ftp",
+		"port": 21,
+		"username":"gamecredit",
+		"password": "oH5kZ4dM0y",
+		"remotePath": "/www/gamecredit.trade",
+		"uploadOnSave": true,
+		"useTempFile": false,
+		"openSsh": false
+	}	
+	
+	
+	
+	{
+		"name": "okupi.exchange",
+		"host": "139.84.174.60",
+		"protocol": "sftp",
+		"port": 22,
+		"username":"okupiswap",
+		"password": "Okupi@1234#",
+		"remotePath": "/public_html",
+		"uploadOnSave": true,
+		"useTempFile": false,
+		"openSsh": false
+	}
+	
+	
+	{
+		"name": "eyadsmm.com",
+		"host": "77.37.37.16",
+		"protocol": "ftp",
+		"port": 21,
+		"username":"u742971103.eyadsmm.com",
+		"password": "mRKnMo1Wkbl?E9l/",
+		"remotePath": "/public_html",
+		"uploadOnSave": true,
+		"useTempFile": false,
+		"openSsh": false
+	}		
+		
+		
+	{
+		"name": "carpcoin.net",
+		"host": "azimut01.ftp.tools",
+		"protocol": "sftp",
+		"port": 22,
+		"username": "azimut01",
+		"password": "6H8Ac4b3Rn",   
+		"remotePath": "/home/azimut01/carpcoin.net/www",
+		"uploadOnSave": true,
+		"useTempFile": false,
+		"openSsh": false
+	}
+
+
+
+	{
+		"name": "okupi.exchange",
+		"host": "139.84.174.60",
+		"protocol": "sftp",
+		"port": 22,
+		"username":"okupiswap",
+		"password": "Okupi@1234#",
+		"remotePath": "/public_html",
+		"uploadOnSave": true,
+		"useTempFile": false,
+		"openSsh": false
+	}
+	
+	
+	Ticket ID #1475465779
+	{
+		"name": "profstake.com",
+		"host": "profstake.com",
+		"protocol": "ftp",
+		"port": 21,
+		"username":"bugfinder_profstake.com",
+		"password": "jc47nK7TK*",
+		"remotePath": "/",
+		"uploadOnSave": true,
+		"useTempFile": false,
+		"openSsh": false
+	}
+	
+	
+	Ticket ID #74419536
+
+	
+	{
+		"name": "famososmm.com",
+		"host": "198.178.122.146",
+		"protocol": "ftp",
+		"port": 21,
+		"username":"famososmm.com_9ujpdjjgk95",
+		"password": "9%1ClqJTlq0jrst#",
+		"remotePath": "/httpdocs",
+		"uploadOnSave": true,
+		"useTempFile": false,
+		"openSsh": false
+	}
+	
+	
+	
+	{
+		"name": "alayman.co",
+		"host": "ftp.aeicoksa.com",
+		"protocol": "ftp",
+		"port": 21,
+		"username":"alayman@alayman.co",
+		"password": "d#(2kaQZ-XHNbzSs",
+		"remotePath": "/",
+		"uploadOnSave": true,
+		"useTempFile": false,
+		"openSsh": false
+	}
+	
+	
+	{
+		"name": "testezupi.store",
+		"host": "147.93.17.170",
+		"protocol": "ftp",
+		"port": 21,
+		"username":"u569563947.testezupi.store",
+		"password": "Deepan2003@",
+		"remotePath": "/",
+		"uploadOnSave": true,
+		"useTempFile": false,
+		"openSsh": false
+	}
+	
+	{
+	  "name": "My Bluehost FTP",
+	  "host": "ftp.wpq.oov.mybluehost.me",
+	  "protocol": "ftp",
+	  "port": 21,
+	  "username": "suport@website-eb9f9929.wpq.oov.mybluehost.me",
+	  "password": "cZPNUYX#l4d$",
+	  "remotePath": "/",
+	  "uploadOnSave": true,
+	  "useTempFile": false,
+	  "openSsh": false
+	}
+
+	{
+    "name": "http://seklia.com/",
+    "host": "ftp.sc3feal7825.universe.wf",
+    "protocol": "ftp",
+    "port": 21,
+    "username": "bugfinder@seklia.com",
+    "password": "mW$vl;?iIU2m",
+    "remotePath": "/",
+    "uploadOnSave": true,
+    "useTempFile": false,
+    "openSsh": false
+	}
+
+
+
+	{
+	  "name": "e-stores",
+	  "host": "ftp.menodev.website",
+	  "protocol": "ftp",
+	  "port": 21,
+	  "username": "e-stores.online@e-stores.online",
+	  "password": "r!$mj3uiKa@j",
+	  "remotePath": "/",
+	  "uploadOnSave": true,
+	  "useTempFile": false,
+	  "openSsh": false
+	}
+
+
+	{
+		"name": "diyi.pro",
+		"host": "83.229.120.232",
+		"protocol": "sftp",
+		"port": 22,
+		"username": "root",
+		"password": "72a3RZDgda718uL1kDd",     
+		"remotePath": "/www/wwwroot/diyi.pro",
+		"uploadOnSave": true,
+		"useTempFile": false,
+		"openSsh": true
+	}
+
+	{
+		"name": "golalabill",
+		"host": "golalabill.com",
+		"protocol": "ftp",
+		"port": 21,
+		"username": "golalabill_golalabill.com",
+		"password": "R7U51oGav@",
+		"remotePath": "/",
+		"uploadOnSave": true,
+		"useTempFile": false,
+		"openSsh": false
+	}
+
+
+	{
+		"name": "bestlivingstore",
+		"host": "ftp.bestlivingstore.com",
+		"protocol": "ftp",
+		"port": 21,
+		"username": "u557742573.bugfinder",
+		"password": "Bugfinder111@",
+		"remotePath": "/",
+		"uploadOnSave": true,
+		"useTempFile": false,
+		"openSsh": false
+	}
+
+
+{
+    "name": "seguidores.app.br",
+    "host": "37.27.246.211",
+    "protocol": "ftp",
+    "port": 21,
+    "username": "seguidoresappbr",
+    "password": "YQte88oZBjsvs",
+    "remotePath": "/public_html",
+    "uploadOnSave": true,
+    "useTempFile": false,
+    "openSsh": false
+}
+
+
+{
+    "name": "MyServer",
+    "host": "46.202.140.44",
+    "protocol": "sftp",
+    "port": 22,
+    "username": "zed",
+    "password": "123456789",
+    "remotePath": "/",
+    "uploadOnSave": true,
+    "useTempFile": false,
+    "openSsh": false
+}
+
+{
+  "name": "MyServer",
+  "host": "50.191.30.118",
+  "protocol": "sftp",
+  "port": 22,
+  "username": "admin-8200",
+  "password": "11501PxIloO@UsW319",
+  "remotePath": "/",
+  "uploadOnSave": true,
+  "useTempFile": false,
+  "openSsh": false
+}
+
+
+{
+  "name": "cPanel FTP",
+  "host": "dns66.dofindings.com",
+  "protocol": "ftp",
+  "port": 21,
+  "username": "dev_satmk",
+  "password": "Development@123",
+  "remotePath": "/",
+  "uploadOnSave": true,
+  "useTempFile": false,
+  "openSsh": false
+}
+
+{
+    "name": "eyadsmm-ftp",
+    "host": "77.37.37.16",
+    "protocol": "ftp",
+    "port": 21,
+    "username": "u742971103.eyadsmm.com",
+    "password": "[f>wO:58D&dClcI9",
+    "remotePath": "/",
+    "uploadOnSave": true,
+    "useTempFile": false,
+    "openSsh": false,
+    "ignore": [
+        ".vscode",
+        ".git",
+        ".DS_Store",
+        ".idea",
+        "vendor",
+        "node_modules"
+    ]
+}
+
+
+
+	
+{
+    "name": "",
+    "host": "",
+    "protocol": "",
+    "port": 22,
+    "username": "",
+    "password": "",
+    "remotePath": "/",
+    "uploadOnSave": true,
+    "useTempFile": false,
+    "openSsh": false,
+    "ignore": [
+        ".vscode",
+        ".git",
+        ".DS_Store",
+        ".idea",
+        ".vscode",
+        "vendor",
+        "node_modules"
+    ]
+}
+FTP Details:
+
+Port:
+FTP (21)
+Host: hvv.upa.mybluehost.me
+User: sada@sadalist.com
+Pass: 3%7W94w9gh
+
+configar it
+
